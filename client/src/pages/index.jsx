2b814655f161eaae3f575020fd62d6f0a2f2d0da
Daniel Yoo <dacapo0509@gmail.com>
2023-05-26 13:48:33 -0700

    added file extensions to index.jsx imports

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,8 +1,9 @@
-import Home from "./home";
-import Login from "./Login";
-import Register from "./Register";
-import Profile from "./Profile";
-import PlaceInfo from "./PlaceInfo";
-import Bookings from "./Bookings";
-import SingleBooking from "./SingleBooking";
+import Home from "./home.jsx";
+import Login from "./Login.jsx";
+import Register from "./Register.jsx";
+import Profile from "./Profile.jsx";
+import PlaceInfo from "./PlaceInfo.jsx";
+import Bookings from "./Bookings.jsx";
+import SingleBooking from "./SingleBooking.jsx";
+
 export { Home, Login, Register, Profile, PlaceInfo, Bookings, SingleBooking };
--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
 	cors({
 		credentials: true,
-		origin: "http://127.0.0.1:5173",
+		origin: "http://127.0.0.1:4173",
 	})
 );
 app.use("/users", userRoutes);
